d868351e8e1425624e12697b37a26cd19949460b
Gabriel <114709637+gblrodrigues@users.noreply.github.com>
2026-06-14 05:50:16 -0300

    feat: make allowed mobs configurable

--- src/main/java/com/gblrod/automobclick/AutoMobClickPlugin.java
+++ src/main/java/com/gblrod/automobclick/AutoMobClickPlugin.java
@@ -1,27 +1,35 @@
 package com.gblrod.automobclick;
 
 import com.gblrod.automobclick.command.AutoMobClickCommand;
+import com.gblrod.automobclick.config.MobConfigurationService;
 import com.gblrod.automobclick.listener.MobDeathListener;
 import com.gblrod.automobclick.presentation.AutoclickMessageService;
 import com.gblrod.automobclick.presentation.StatisticsMessageService;
 import com.gblrod.automobclick.service.AutoMobClickService;
 import com.gblrod.automobclick.service.MobStatisticsService;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 
 public class AutoMobClickPlugin extends JavaPlugin {
-
     private AutoMobClickService autoClickService;
-    private MobStatisticsService statisticsService;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        MobConfigurationService mobConfigurationService = new MobConfigurationService(getConfig(), getLogger());
         AutoclickMessageService autoclickMessageService = new AutoclickMessageService();
+        Set<EntityType> allowedMobs = mobConfigurationService.getAllowedMobs();
 
-        autoClickService = new AutoMobClickService(this, autoclickMessageService);
-        statisticsService = new MobStatisticsService();
+        autoClickService = new AutoMobClickService(
+                this,
+                autoclickMessageService,
+                allowedMobs
+        );
+        MobStatisticsService statisticsService = new MobStatisticsService();
 
         StatisticsMessageService statisticsMessageService = new StatisticsMessageService(statisticsService);
 
@@ -34,7 +42,8 @@ public class AutoMobClickPlugin extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new MobDeathListener(
                         statisticsService,
-                        autoClickService
+                        autoClickService,
+                        allowedMobs
                 ),
                 this
         );
